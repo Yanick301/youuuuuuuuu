415b61c78f46323c6f76c22047e19e2316a84f4f
Ddm Martin <martinddm301@gmail.com>
2025-11-23 21:03:31 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8762209881-fa482",
+  "appId": "1:188715334152:web:4c1edd6a3043cd6f806fbd",
+  "apiKey": "AIzaSyDZ2GLM3V8LmtKhwfPj84Yt4DL3lTRT1KI",
+  "authDomain": "studio-8762209881-fa482.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "188715334152"
+};
