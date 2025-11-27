584431a5bfb3758197b9801cfb00acd1e3784a47
Ddm Martin <martinddm301@gmail.com>
2025-11-27 16:32:33 +0000

    verifie chaque contenu du site et dit moi s'il y a quelque chose qui peu

--- next.config.ts
+++ next.config.ts
@@ -4,16 +4,22 @@ import type {NextConfig} from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   typescript: {
-    ignoreBuildErrors: true,
+    ignoreBuildErrors: false,
   },
   eslint: {
-    ignoreDuringBuilds: true,
+    ignoreDuringBuilds: false,
   },
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+       {
+        protocol: 'https',
+        hostname: 'picsum.photos',
         port: '',
         pathname: '/**',
       },
